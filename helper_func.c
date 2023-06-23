94a4eca6e55cd8525806ccb9ca77687644906b09
TeeMeeLehin <samemerald8@gmail.com>
2023-06-23 03:14:26 +0100

    fixes

--- helper_func.c
+++ helper_func.c
@@ -18,3 +18,53 @@ i++;
 }
 inputs[i] = NULL;
 }
+
+/**
+* swap - swapping the top two elements of the stack
+* @line_no: line number of instruction
+* @rack: the input stack
+* Return: void
+*/
+void swap(stack_t **rack, unsigned int line_no)
+{
+stack_t *temp = *rack;
+int n;
+if (*rack == NULL || (*rack)->next == NULL)
+{
+fprintf(stderr, "L%d: can't swap, stack too short", line_no);
+exit(EXIT_FAILURE);
+}
+n = temp->n;
+temp->n = temp->next->n;
+temp->next->n = n;
+}
+
+/**
+* nop - does nothing
+* Return: void
+*/
+void nop(void)
+{
+return;
+}
+
+/**
+* add - adding the top two elements of the stack
+* @line_no: line number of instruction
+* @rack: the input stack
+* Return: void
+*/
+void add(stack_t **rack, unsigned int line_no)
+{
+stack_t *temp = *rack;
+int n;
+if (*rack == NULL || (*rack)->next == NULL)
+{
+fprintf(stderr, "L%d: can't swap, stack too short", line_no);
+exit(EXIT_FAILURE);
+}
+n = temp->n;
+temp->n = temp->next->n;
+temp->next->n = temp->next->n + n;
+pop(rack, line_no);
+}
